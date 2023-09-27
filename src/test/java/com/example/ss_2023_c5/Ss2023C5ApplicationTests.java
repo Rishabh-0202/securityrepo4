50ca09e1354e79b9c969c4dece57d0f6f2866097
Rishabh <rishabhtripathi0202@gmail.com>
2023-09-27 23:10:59 +0530

    first commit

---
+++ src/test/java/com/example/ss_2023_c5/Ss2023C5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ss_2023_c5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss2023C5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
